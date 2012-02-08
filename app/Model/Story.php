08183a9abede8460fd49ceacd6acbfd1c8c81f71
Simon Males <sime@sime.net.au>
2012-02-09 10:39:08 +1100

    Story model

---
+++ app/Model/Story.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Story Model
+ *
+ */
+class Story extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'title';
+}
