35727f7ae293f30e70c84143347656b0e9c64d3c
Simon Males <you@example.com>
2012-02-09 12:23:23 +1100

    News tables

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -1,5 +1,5 @@
 <?php 
-/* generated on: 2012-02-09 12:20:09 : 1328750409 */
+/* generated on: 2012-02-09 12:22:54 : 1328750574 */
 class AppSchema extends CakeSchema {
 
 	public function before($event = array()) {
@@ -9,6 +9,11 @@ class AppSchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
+	public $news = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
 	public $stories = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
 		'title' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
