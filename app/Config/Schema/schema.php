1f5d3bcd52a22abb08ab3a03632269c3beaea364
Simon Males <you@example.com>
2012-02-09 12:20:28 +1100

    Pull out email for twitter

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -1,5 +1,5 @@
 <?php 
-/* generated on: 2012-02-09 12:17:49 : 1328750269 */
+/* generated on: 2012-02-09 12:20:09 : 1328750409 */
 class AppSchema extends CakeSchema {
 
 	public function before($event = array()) {
@@ -19,7 +19,7 @@ class AppSchema extends CakeSchema {
 	public $users = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
 		'username' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'email' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+		'twitter' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
 		'password' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 45, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
